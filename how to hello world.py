74359dc10f236b5132d888b5df1aecc0667a2947
Roach-2398 <103074039+Roach-2398@users.noreply.github.com>
2022-04-05 17:14:20 -0400

    Add files via upload

---
+++ how to hello world.py
@@ -0,0 +1,43 @@
+#This script should be a fairly simple python tutorial for hello world written in python.
+print("hello world")
+
+a = input("What is your name?")
+
+print("Hello", a, "Welcome to online-python.com. We hope you enjoy it here!")
+
+b = input("Do you know how to write Hello World in python?")
+
+if b == "yes":
+    print("Great! lets do it.")
+else:
+    print("Ok then, You can use 'print' to put words in the terminal.")
+    print("Write it like this...")
+    print(" print('hello world') ")
+    print("hello world")
+    print("Just like that, now you try!")
+    
+    
+c = input("Write the Code for hello world: ")
+
+if c == "print('hello world')":
+    print("Nice Job!!! Now you can move on and learn more about python!")
+    print("cya later!")
+    print("ignore any text below this line")
+else:
+    print("not quite, make sure it looks like this...")
+    print("print('hello world')")
+    d = input("Write the code for Hello World: ")
+    
+
+
+if d == "print('hello world')":
+    print("Nice Job!!! Now you can move on and learn more about python!")
+    print("cya later!")
+else:
+    print("Nope, that isnt it... Try again another day!")
+    print("For now, cya later!")
+    print("Note From Dev: if you wrote hello world using quotations it will not work in this project even though it tecnichally would in regular python.")
+    print("Also make sure that everything is lowercase! And as always, I appologize if this caused you any inconvenience. bye")
+    
+    
+    
